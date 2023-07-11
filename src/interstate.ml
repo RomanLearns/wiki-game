b04d9b3cf16e85e98f80d38f39233c0db27f371f
Roman Chenoweth <romanc22@vt.edu>
2023-07-11 21:12:55 +0000

    interstate in progress, via Lydia and Eric

--- src/interstate.ml
+++ src/interstate.ml
@@ -1,30 +1,25 @@
 open! Core
-module Location = String
-(* We separate out the [Network] module to represent our highway connections in OCaml types. *)
-module Network = struct
-  (* We can represent our interstate as a set of connections, where a connection
-     represents a road between two places. *)
-  module Connection = struct
-    module T = struct
-      type t = Location.t * Location.t [@@deriving compare, sexp]
-    end
 
-  type t = Connection.Set.t [@@deriving sexp_of]
+let rec tuples ~locations ~highway ~accumulator =
+  match locations with
+  | city_1 :: city_2 :: tail ->
+    tuples
+      ~highway
+      ~locations:([ city_2 ] @ tail)
+      ~accumulator:
+        (accumulator
+         @ [ city_1, city_2, highway ]
+         @ [ city_2, city_1, highway ])
+  | _ -> accumulator
+;;
 
 let of_file input_file =
-  let connections =
-    In_channel.read_lines (File_path.to_string input_file)
-    |> List.concat_map ~f:(fun s ->
-      match Connection.of_string s with
-      | Some (a, b) ->
-        (* Friendships are mutual; a connection between a and b means we should also
-           consider the connection between b and a. *)
-        [ a, b; b, a ]
-      | None ->
-        printf "ERROR: Could not parse line as connection; dropping. %s\n" s;
-        [])
-  in
-  Connection.Set.of_list connections
+  In_channel.read_lines (File_path.to_string input_file)
+  |> List.concat_map ~f:(fun line ->
+       let list = String.split line ~on:',' in
+       let highway = List.hd_exn list in
+       let cities = List.tl_exn list in
+       tuples ~locations:cities ~highway ~accumulator:[])
 ;;
 
 let load_command =
@@ -36,25 +31,28 @@ let load_command =
         flag
           "input"
           (required File_path.arg_type)
-          ~doc:"FILE a file listing interstates and the cities they go through"
+          ~doc:
+            "FILE a file listing interstates and the cities they go through"
       in
       fun () ->
-        ignore (input_file : File_path.t);
-        failwith "TODO"]
+        let network = of_file input_file in
+        print_s [%sexp (network : (string * string * string) list)]]
 ;;
 
 let visualize_command =
   let open Command.Let_syntax in
   Command.basic
     ~summary:
-      "parse a file listing interstates and generate a graph visualizing the highway \
-       network"
+      "parse a file listing interstates and generate a graph visualizing \
+       the highway network"
     [%map_open
       let input_file =
         flag
           "input"
           (required File_path.arg_type)
-          ~doc:"FILE a file listing all interstates and the cities they go through"
+          ~doc:
+            "FILE a file listing all interstates and the cities they go \
+             through"
       and output_file =
         flag
           "output"
